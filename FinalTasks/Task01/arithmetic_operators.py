5038ca904d1cbccdbafbf2108a665e8bb9197696
Aytan Rustamzada <54712645+AytanRustamova@users.noreply.github.com>
2021-12-03 19:00:14 +0300

    Update arithmetic_operators.py

--- FinalTasks/Task01/arithmetic_operators.py
+++ FinalTasks/Task01/arithmetic_operators.py
@@ -9,6 +9,8 @@ def calculateDifference(num1, num2):
 
 def calculateProduct(num1, num2):
     print(num1*num2)
+    
+ # bu problem üçün 3 ayrı funksiya yaratmağa ehtiyac yoxdur. 1 funksiyanın içində bütün printləri yazmaq olar.
 
 calculateSum(a,b)
 calculateDifference(a,b)
